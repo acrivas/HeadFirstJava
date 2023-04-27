19ec81724aa85fb56f3ab90e9876cfd09f0596b3
acrivas <ac.rivas.dev@gmail.com>
2023-04-27 06:20:08 -0400

    Chapter 3 be the compiler

--- Chapter03/C03Ex1BeTheCompiler/A/BooksTestDrive.java
+++ Chapter03/C03Ex1BeTheCompiler/A/BooksTestDrive.java
@@ -1,6 +1,9 @@
 class BooksTestDrive {
 	public static void main(String[] args) {
 		Books[] myBooks = new Books[3];
+		myBooks[0] = new Books();
+		myBooks[1] = new Books();
+		myBooks[2] = new Books();
 		int x = 0;
 		myBooks[0].title = "The Grapes of Java";
 		myBooks[1].title = "The Java Gatsby";
@@ -10,7 +13,7 @@ class BooksTestDrive {
 		myBooks[2].author = "ian";
 
 		while (x < 3) {
-			System.out.pring(myBooks[x].title);
+			System.out.print(myBooks[x].title);
 			System.out.print(" by ");
 			System.out.println(myBooks[x].author);
 			x = x + 1;
